d52a2ad94bfe2440819278c26d94957c6fff7833
Julio de Leon <jangelod@gmail.com>
2017-05-02 05:56:15 -0500

    Not probing the Rows correctly but have input from keyboard

--- KeyboardTestingMatrix/KeyboardTestingMatrix.ino
+++ KeyboardTestingMatrix/KeyboardTestingMatrix.ino
@@ -1,21 +1,8 @@
+#include "types.h"
+
 int inputs[] = {2,3,4,5,6,7,8,14,15,16,17,18,19,20,21,22,23};
 
-const int ROWS[] = {2,3,4,5,6};
-const int NUM_ROWS = 5;
-
-const int COLS[] = {7,8,14,15,16,18,17,19,20,21,22,23};
-const int NUM_COLS = 12;
 int OUTPUTS[NUM_ROWS][NUM_COLS] = {{0}};
-
-//
-//void printOutputs(int* outputs) {
-//  char* outputStr = (char*) malloc(sizeof(char) * 64);
-//  sprintf(outputStr, "2: %d 3: %d 4: %d 5: %d 6: %d 7: %d 8: %d 14: %d 15: %d 16: %d 17: %d 18: %d 19: %d 20: %d 21: %d 22: %d 23: %d",
-//    outputs[0], outputs[1], outputs[2], outputs[3], outputs[4], outputs[5], outputs[6], outputs[7], outputs[8], outputs[9], 
-//    outputs[10], outputs[11], outputs[12], outputs[13], outputs[14], outputs[15], outputs[16]);
-//  Serial.println(outputStr);
-//  free(outputStr);
-//}
 
 void resetOutputs() {
   for (int x = 0; x < NUM_ROWS; x++) {
@@ -42,19 +29,7 @@ void setup() {
   resetOutputs();
 }
 
-void scanColumn(int col) {
-  for(int x = 0; x < NUM_COLS; x++) {
-    if (x != col) {
-      digitalWrite(COLS[x], LOW);
-    } else {
-      digitalWrite(COLS[x], HIGH);
-    }
-  }
 
-  for(int x = 0; x < NUM_ROWS; x++) {
-    OUTPUTS[x][col] = digitalRead(ROWS[x]);
-  }
-}
 
 void scanRow(int row) {
   for(int x = 0; x < NUM_ROWS; x++) {
@@ -65,15 +40,27 @@ void scanRow(int row) {
     }
   }
 
+  int output = 0;
   for(int x = 0; x < NUM_COLS; x++) {
     OUTPUTS[row][x] = digitalRead(COLS[x]);
+    output = output + (OUTPUTS[row][x] << x);
   }
+  
+  if ( output != 4095) {
+    for(int x = 0; x < NUM_COLS; x++) {
+      if( 1 and (output >> x) ){
+        Serial.printf("HERE is: %dx%d: %c at %d\n", row, x, DEFAULT_FACE[row][x], output);   
+      }
+    }
+  }
+  
 }
 
 void readMatrix() {
   for(int x = 0; x < NUM_ROWS; x++) {
   //  scanColumn(x);
     scanRow(x);
+    resetOutputs();
   }
 }
 
@@ -92,7 +79,7 @@ void printMatrix() {
 void loop() {
   resetOutputs();
   readMatrix();
-  printMatrix();
+  //printMatrix();
 }
 
 
---
+++ KeyboardTestingMatrix/types.h
@@ -0,0 +1,18 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+const int ROWS[] = {2,3,4,5,6};
+const int NUM_ROWS = 5;
+const int COLS[] = {7,8,14,15,16,18,17,19,20,21,22,23};
+const int NUM_COLS = 12;
+
+const char DEFAULT_FACE[NUM_ROWS][NUM_COLS] = {
+  {0,'1','2','3','4','5','6','7','8','9','0',0},
+  {0,'q','w','e','r','t','y','u','i','o','p',0},
+  {0,'a','s','d','f','g','h','j','k','l',';',0},
+  {0,'z','x','c','v','b','n','m',',',',','/',0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,}
+};
+
+
+#endif
