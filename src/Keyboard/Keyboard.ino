ade450d2efd250ad9857ac9b70fc967ac78c6298
Julio de Leon <jangelod@gmail.com>
2017-05-08 22:03:10 -0500

    worked out modifier logic, define different layers and optimize reads

--- src/Keyboard/Keyboard.ino
+++ src/Keyboard/Keyboard.ino
@@ -1,9 +1,8 @@
 #include "types.h"
 
-/*
+/*n7ujm
  * a
  */
-
 void setup() {
   //start serial connection for printing
   Serial.begin(9600);
@@ -15,9 +14,70 @@ void setup() {
 
   for(int x = 0; x < NUM_COLS; x++) {
     pinMode(COLS[x], INPUT_PULLUP);
-    
   }
 }
+
+
+int row = 0;
+int col = 0;
+
+void loop() {
+  //readMatrix();
+  //printMatrix();
+  Keyboard.set_modifier(checkModifiers());
+  setRow(row); 
+  if (checkKey(row,col)) {
+    Serial.printf("%dx%d has been pressed: %c\n", row,col,DEFAULT_FACE[row][col]);
+    //Keyboard.print(DEFAULT_FACE[row][col]);
+    Keyboard.press(DEFAULT_FACE[row][col]);
+    delay(150);
+
+  } else {
+    Keyboard.release(DEFAULT_FACE[row][col]);
+  }
+
+  row = (row + 1) % NUM_ROWS;
+  if (row == NUM_ROWS -1) {
+    col = (col + 1) % NUM_COLS; 
+  }
+}
+
+
+
+/*
+ * 
+ * 
+ */
+
+ layer checkLayer() {
+  bool r = checkKey(RAISE_MOD_ROW, RAISE_MOD_COL);
+  bool l = checkKey(LOWER_MOD_ROW, LOWER_MOD_COL);
+  if( not (r xor l) ) {
+    return BASE;
+  } else if ( r and not l) {
+    return RAISE;
+  } else if ( l and not r) {
+    return LOWER;
+  } else {
+    return BASE;
+  }
+}
+
+int checkModifiers() {
+  int ret = 0;
+  ret |= (checkKey(LEFT_CTRL_ROW,LEFT_CTRL_COL))? MODIFIERKEY_CTRL : 0;
+  ret |= (checkKey(LEFT_ALT_ROW,LEFT_ALT_COL))? MODIFIERKEY_ALT : 0;
+  ret |= (checkKey(LEFT_SHIFT_ROW,LEFT_SHIFT_COL))? MODIFIERKEY_SHIFT : 0;
+  ret |= ret | (checkKey(LEFT_GUI_ROW,LEFT_GUI_COL))? MODIFIERKEY_GUI : 0;
+  return ret;
+}
+
+bool checkKey(int rowP, int colP) {
+  setRow(rowP);
+  int output = digitalRead(COLS[colP]);
+  return output == LOW;
+}
+
 
 void setRow(int rowp) {
    for(int x = 0; x < NUM_ROWS; x++) {
@@ -30,26 +90,3 @@ void setRow(int rowp) {
     }
   }
 }
-
-int row = 0;
-int col = 0;
-
-void loop() {
-  //readMatrix();
-  //printMatrix();
-   setRow(row); 
-  int output = digitalRead(COLS[col]);
-  if (output == LOW) {
-    Serial.printf("%dx%d has been pressed: %c\n", row,col,DEFAULT_FACE[row][col]);
-    //Keyboard.print(DEFAULT_FACE[row][col]);
-    Keyboard.press(DEFAULT_FACE[row][col]);
-    delay(150);
-  } else {
-    Keyboard.release(DEFAULT_FACE[row][col]);
-  }
-  row = (row + 1) % NUM_ROWS;
-  if (row == NUM_ROWS -1) {
-    col = (col + 1) % NUM_COLS; 
-  }
-}
-
--- src/Keyboard/types.h
+++ src/Keyboard/types.h
@@ -1,18 +1,42 @@
 #ifndef TYPES_H
 #define TYPES_H
+#include <Bounce2.h>
+#define u_long unsigned long
 
+const u_long BOUNCE_TIME = 5;
 const int ROWS[] = {2,3,4,5,6};
 const int NUM_ROWS = 5;
 const int COLS[] = {7,8,14,15,16,18,17,19,20,21,22,23};
 const int NUM_COLS = 12;
+
+const int LEFT_CTRL_COL = 0;
+const int LEFT_CTRL_ROW = 4;
+
+const int LEFT_ALT_COL = 2;
+const int LEFT_ALT_ROW = 4;
+
+const int LEFT_GUI_COL = 1;
+const int LEFT_GUI_ROW = 4;
+
 const int LEFT_FUNCTION_COL = 3;
+const int LEFT_FUNCTION_ROW = 4;
+
 const int LOWER_MOD_COL = 4;
+const int LOWER_MOD_ROW = 4;
+
 const int RAISE_MOD_COL = 7;
+const int RAISE_MOD_ROW = 4;
+
+const int LEFT_SHIFT_COL = 0;
+const int LEFT_SHIFT_ROW = 3;
+
+enum layer { BASE, LOWER, RAISE };
+
 const int DEFAULT_FACE[NUM_ROWS][NUM_COLS] = {
   {KEY_ESC,KEY_1,KEY_2,KEY_3,KEY_4,KEY_5,KEY_6,KEY_7,KEY_8,KEY_9,KEY_0,KEY_BACKSPACE},
   {KEY_TAB,KEY_Q,KEY_W,KEY_E,KEY_R,KEY_T,KEY_Y,KEY_U,KEY_I,KEY_O,KEY_P,KEY_BACKSLASH},
   {0,KEY_A,KEY_S,KEY_D,KEY_F,KEY_G,KEY_H,KEY_J,KEY_K,KEY_L,KEY_SEMICOLON,KEY_QUOTE},
-  {0,KEY_Z,KEY_X,KEY_C,KEY_V,KEY_B,KEY_N,KEY_M,KEY_COMMA,KEY_PERIOD,KEY_SLASH,0},
+  {KEY_LEFT_SHIFT,KEY_Z,KEY_X,KEY_C,KEY_V,KEY_B,KEY_N,KEY_M,KEY_COMMA,KEY_PERIOD,KEY_SLASH,0},
   {KEY_LEFT_CTRL,KEY_LEFT_GUI,KEY_LEFT_ALT,0,0,KEY_SPACE,KEY_ENTER,0,KEY_LEFT,KEY_DOWN,KEY_UP,KEY_RIGHT}
 };
 
@@ -24,5 +48,11 @@ const int FUNCTION_FACE[NUM_ROWS][NUM_COLS] = {
   {0,0,0,0,0,0,0,0,0,0,0,0,}
 };
 
+
+// util functions
+layer checkLayer();
+int checkModifiers();
+bool checkKey(int row, int col);
+void setRow(int row);
 
 #endif
